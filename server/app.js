da9a06f5ee5e04b745e6d5efdce4c3c457a5ee8d
SoumyaSubhrajit <soumyasubhrajit@gmail.com~>
2024-01-03 13:51:20 +0530

    Adding some more  file to the repo

--- server/app.js
+++ server/app.js
@@ -1,16 +1,20 @@
 const express = require('express');
-
-
-
-
+const mongoose = require('mongoose');
+const cors = require('cores');
+const connectDB = require('./db');
+const dotenv = require('dotenv')
+PORT = process.env.PORT || 4001
 const app = express();
 
+
+dotenv.config();
+
 app.get('/', (req,res)=> {
-    res.send("Hello from AeroVue team");
+    res.send("Hello from AeroVue team :)");
 
 })
 
-PORT = 4000;
+connectDB();
 app.listen(PORT, ()=>{
     console.log(`Server is running on port ${PORT}`)
 })
